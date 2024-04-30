a5d1a2c159ff45e85ef26a80f4edd87fcdccc193
shalevfridman <146954691+shalevfridman@users.noreply.github.com>
2024-04-30 22:51:06 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,227 @@
+﻿using System;
+
+namespace FractionCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor originalForeground = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(@"               .__                                  __             __  .__                        
+__  _  __ ____ |  |   ____  ____   _____   ____   _/  |_  ____   _/  |_|  |__   ____              
+\ \/ \/ // __ \|  | _/ ___\/  _ \ /     \_/ __ \  \   __\/  _ \  \   __\  |  \_/ __ \             
+ \     /\  ___/|  |_\  \__(  <_> )  Y Y  \  ___/   |  | (  <_> )  |  | |   Y  \  ___/             
+  \/\_/  \___  >____/\___  >____/|__|_|  /\___  >  |__|  \____/   |__| |___|  /\___  >            
+             \/          \/            \/     \/                            \/     \/             
+  _________.__               .__           ___________                     __  .__                
+ /   _____/|__| _____ ______ |  |   ____   \_   _____/___________    _____/  |_|__| ____   ____   
+ \_____  \ |  |/     \\____ \|  | _/ __ \   |    __) \_  __ \__  \ _/ ___\   __\  |/  _ \ /    \  
+/        \|  |  Y Y  \  |_> >  |_\  ___/   |     \   |  | \// __ \\  \___|  | |  (  <_> )   |  \ 
+/_______  /|__|__|_|  /   __/|____/\___  >  \___  /   |__|  (____  /\___  >__| |__|\____/|___|  / 
+        \/          \/|__|             \/       \/               \/     \/                    \/  
+_________        .__               .__          __                                                
+\_   ___ \_____  |  |   ____  __ __|  | _____ _/  |_  ___________                                 
+/    \  \/\__  \ |  | _/ ___\|  |  \  | \__  \\   __\/  _ \_  __ \                                
+\     \____/ __ \|  |_\  \___|  |  /  |__/ __ \|  | (  <_> )  | \/                                
+ \______  (____  /____/\___  >____/|____(____  /__|  \____/|__|                                   
+        \/     \/          \/                \/                                                   ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.ForegroundColor = originalForeground;
+
+            Console.WriteLine("Welcome to the Simple Fraction Calculator!");
+            Console.WriteLine("Please choose an option by entering the corresponding number:");
+
+            Console.WriteLine("1. Reduce a fraction");
+            Console.WriteLine("2. Add fractions");
+            Console.WriteLine("3. Subtract fractions");
+            Console.WriteLine("4. Multiply fractions");
+            Console.WriteLine("5. Divide fractions");
+            Console.WriteLine("6. Exit");
+
+            int userChoice = int.Parse(Console.ReadLine());
+            ConsoleColor originalForegroundColor = Console.ForegroundColor;
+            ConsoleColor originalBackgroundColor = Console.BackgroundColor;
+            int originalCursorLeft = Console.CursorLeft;
+            int originalCursorTop = Console.CursorTop;
+
+            while (userChoice != 6)
+            {
+                originalForegroundColor = Console.ForegroundColor;
+                int cursorTop = userChoice;
+                originalCursorLeft = Console.CursorLeft;
+                originalCursorTop = Console.CursorTop;
+
+                Console.SetCursorPosition(0, cursorTop - 1);
+
+                originalBackgroundColor = Console.BackgroundColor;
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine($"{userChoice}");
+
+                Console.ForegroundColor = originalForegroundColor;
+                Console.BackgroundColor = originalBackgroundColor;
+                Console.SetCursorPosition(originalCursorLeft, originalCursorTop);
+
+                if (userChoice == 1)
+                {
+                    Console.WriteLine("Please enter the numerator:");
+                    int nominator = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator:");
+                    int denominator = int.Parse(Console.ReadLine());
+                    Fraction frToReduce = new Fraction(nominator, denominator);
+
+                    frToReduce.Reduce();
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("*************************************");
+                    Console.WriteLine($"The reduced fraction is: {frToReduce}");
+                    Console.WriteLine("*************************************");
+                    Console.ForegroundColor = originalForegroundColor;
+                    Console.BackgroundColor = originalBackgroundColor;
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                }
+                else if (userChoice == 2)
+                {
+                    Console.WriteLine("Please enter the numerator of the first fraction:");
+                    int nominator1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the first fraction:");
+                    int denominator1 = int.Parse(Console.ReadLine());
+                    Fraction fraction1 = new Fraction(nominator1, denominator1);
+
+                    Console.WriteLine("Please enter the numerator of the second fraction:");
+                    int nominator2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the second fraction:");
+                    int denominator2 = int.Parse(Console.ReadLine());
+                    Fraction fraction2 = new Fraction(nominator2, denominator2);
+
+                    Fraction result = fraction1.Add(fraction2);
+                    result.Reduce();
+
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("*************************************");
+                    Console.WriteLine($"The result of adding {fraction1} and {fraction2} is: {result}");
+                    Console.WriteLine("*************************************");
+                    Console.ForegroundColor = originalForegroundColor;
+                    Console.BackgroundColor = originalBackgroundColor;
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                }
+                else if (userChoice == 3)
+                {
+                    Console.WriteLine("Please enter the numerator of the first fraction:");
+                    int nominator1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the first fraction:");
+                    int denominator1 = int.Parse(Console.ReadLine());
+                    Fraction fraction1 = new Fraction(nominator1, denominator1);
+
+                    Console.WriteLine("Please enter the numerator of the second fraction:");
+                    int nominator2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the second fraction:");
+                    int denominator2 = int.Parse(Console.ReadLine());
+                    Fraction fraction2 = new Fraction(nominator2, denominator2);
+
+                    Fraction result = fraction1.Subtract(fraction2);
+                    result.Reduce();
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("*************************************");
+                    Console.WriteLine($"The result of subtracting {fraction2} from {fraction1} is: {result}");
+                    Console.WriteLine("*************************************");
+                    Console.ForegroundColor = originalForegroundColor;
+                    Console.BackgroundColor = originalBackgroundColor;
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                }
+                else if (userChoice == 4)
+                {
+                    Console.WriteLine("Please enter the numerator of the first fraction:");
+                    int nominator1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the first fraction:");
+                    int denominator1 = int.Parse(Console.ReadLine());
+                    Fraction fraction1 = new Fraction(nominator1, denominator1);
+
+                    Console.WriteLine("Please enter the numerator of the second fraction:");
+                    int nominator2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the second fraction:");
+                    int denominator2 = int.Parse(Console.ReadLine());
+                    Fraction fraction2 = new Fraction(nominator2, denominator2);
+
+                    Fraction result = fraction1.Multiply(fraction2);
+                    result.Reduce();
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("*************************************");
+                    Console.WriteLine($"The result of multiplying {fraction1} and {fraction2} is: {result}");
+                    Console.WriteLine("*************************************");
+                    Console.ForegroundColor = originalForegroundColor;
+                    Console.BackgroundColor = originalBackgroundColor;
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                }
+                else if (userChoice == 5)
+                {
+                    Console.WriteLine("Please enter the numerator of the first fraction:");
+                    int nominator1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the first fraction:");
+                    int denominator1 = int.Parse(Console.ReadLine());
+                    Fraction fra1 = new Fraction(nominator1, denominator1);
+
+                    Console.WriteLine("Please enter the numerator of the second fraction:");
+                    int nominator2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the denominator of the second fraction:");
+                    int denominator2 = int.Parse(Console.ReadLine());
+                    Fraction fra2 = new Fraction(nominator2, denominator2);
+                    Fraction result = fra1.Divide(fra2);
+                    result.Reduce();
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("*************************************");
+                    Console.WriteLine($"The result of dividing {fra1} by {fra2} is: {result}");
+                    Console.WriteLine("*************************************");
+                    Console.ForegroundColor = originalForegroundColor;
+                    Console.BackgroundColor = originalBackgroundColor;
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadLine();
+                }
+                    Console.Clear();
+                    Console.WriteLine("Welcome to the Simple Fraction Calculator!");
+                    Console.WriteLine("Please choose an option by entering the corresponding number:");
+
+                    Console.WriteLine("1. Reduce a fraction");
+                    Console.WriteLine("2. Add fractions");
+                    Console.WriteLine("3. Subtract fractions");
+                    Console.WriteLine("4. Multiply fractions");
+                    Console.WriteLine("5. Divide fractions");
+                    Console.WriteLine("6. Exit");
+
+                    Console.Write("Enter your choice: ");
+                    userChoice = int.Parse(Console.ReadLine());
+                }
+
+                if (userChoice == 6)
+                {
+                    Console.SetCursorPosition(0, 5);
+
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.WriteLine($"{userChoice}");
+                    Console.ForegroundColor = originalForegroundColor;
+                    Console.BackgroundColor = originalBackgroundColor;
+                    Console.SetCursorPosition(originalCursorLeft, originalCursorTop);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("*************************************");
+                    Console.WriteLine("Thank you for using the Simple Fraction Calculator!");
+                    Console.WriteLine("*************************************");
+                    Console.ForegroundColor = originalForegroundColor;
+                    Console.BackgroundColor = originalBackgroundColor;
+                }
+            }
+        }
+    }
+
+                
---
+++ Simple fraction calculator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Fraction
+{
+    private int numerator;
+    private int denominator;
+    private int wholeNumber;
+
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+        wholeNumber = numerator / denominator;
+        numerator %= denominator;
+    }
+
+    public int Numerator { get { return numerator; } set { numerator = value; } }
+    public int Denominator { get { return denominator; } set { denominator = value; } }
+    public int WholeNumber { get { return wholeNumber; } }
+
+    public override string ToString()
+    {
+        if (wholeNumber > 0)
+            return $"{wholeNumber} {numerator}/{denominator}";
+        else
+            return $"{numerator}/{denominator}";
+    }
+
+    public int GCD(int a, int b)
+    {
+        return b == 0 ? a : GCD(b, a % b);
+    }
+
+    public void Reduce()
+    {
+        int gcd = GCD(Math.Abs(numerator), denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+        wholeNumber = numerator / denominator;
+        numerator %= denominator;
+    }
+
+    public Fraction Add(Fraction other)
+    {
+        int commonDenominator = denominator * other.denominator;
+        int newNumerator = numerator * other.denominator + other.numerator * denominator;
+        return new Fraction(newNumerator, commonDenominator);
+    }
+
+    public Fraction Subtract(Fraction other)
+    {
+        int commonDenominator = denominator * other.denominator;
+        int newNumerator = numerator * other.denominator - other.numerator * denominator;
+        return new Fraction(newNumerator, commonDenominator);
+    }
+
+    public Fraction Multiply(Fraction other)
+    {
+        int newNumerator = numerator * other.numerator;
+        int newDenominator = denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    public Fraction Divide(Fraction other)
+    {
+        int newNumerator = numerator * other.denominator;
+        int newDenominator = denominator * other.numerator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+}
